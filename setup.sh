3fbdd416ea8f99eec72a4523a6af992a1a3bbcb1
yangzhang-roboticist <youngzh@umich.edu>
2020-04-15 10:03:43 -0700

    update setup.sh to install carla

--- setup.sh
+++ setup.sh
@@ -1,25 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # Install dependencies. 
-sudo apt-get update &&
-sudo apt-get install wget software-properties-common &&
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test &&
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add - &&
-sudo apt-add-repository "deb http://apt.llvm.org/$(lsb_release -c --short)/ llvm-toolchain-$(lsb_release -c --short)-8 main" &&
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 304F9BC29914A77D
+sudo add-apt-repository "deb [arch=amd64 trusted=yes] http://dist.carla.org/carla-0.9.8/ all main"
 sudo apt-get update
-
-# Additional dependencies for Ubuntu 18.04.
-sudo apt-get install build-essential clang-8 lld-8 g++-7 cmake ninja-build libvulkan1 python python-pip python-dev python3-dev python3-pip libpng-dev libtiff5-dev libjpeg-dev tzdata sed curl unzip autoconf libtool rsync libxml2-dev &&
-pip2 install --user setuptools &&
-pip3 install --user setuptools 
-
-# Additional dependencies for previous Ubuntu versions. 
-sudo apt-get install build-essential clang-8 lld-8 g++-7 cmake ninja-build libvulkan1 python python-pip python-dev python3-dev python3-pip libpng16-dev libtiff5-dev libjpeg-dev tzdata sed curl unzip autoconf libtool rsync libxml2-dev &&
-pip2 install --user setuptools &&
-pip3 install --user setuptools 
-
-# Change default clang version.
-sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/lib/llvm-8/bin/clang++ 180 &&
-sudo update-alternatives --install /usr/bin/clang clang /usr/lib/llvm-8/bin/clang 180
-
-# Clone carla and unreal engine git repos
-git submodule update --init --recursive
+sudo apt-get install carla
+echo "alias carla='/opt/carla/bin/CarlaUE4.sh'" >> ~/.bashrc
+source ~/.bashrc

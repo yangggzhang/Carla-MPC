8ba447de4495e751bd10e42f3ea899b28a6416ec
young <youngzh@umich.edu>
2020-05-07 11:34:16 -0700

    updated first order eular

--- scripts/jaco.m
+++ scripts/jaco.m
@@ -1,3 +1,7 @@
 syms v psi lr lf delta a L x y
-A = jacobian([ v*cos(psi + atan(lr/L*tan(delta))), v*sin(psi + atan(lr/L*tan(delta))), a, v/lr*sin(atan(lr/L*tan(delta)))], [x, y, v, psi]);
-B = jacobian([ v*cos(psi + atan(lr/L*tan(delta))), v*sin(psi + atan(lr/L*tan(delta))), a, v/lr*sin(atan(lr/L*tan(delta)))], [a, delta]);
+f = [ v*cos(psi + atan(lr/L*tan(delta))); v*sin(psi + atan(lr/L*tan(delta))); a; v/lr*sin(atan(lr/L*tan(delta)))];
+A = jacobian(f, [x; y; v; psi]);
+B = jacobian(f, [a; delta]);
+X = [x; y; v; psi];
+U = [a; delta];
+C = f - A*X - B*U;

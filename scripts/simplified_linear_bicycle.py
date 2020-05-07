c6f56236ff0cfe4695fc3bbb9b1ef044230f80b7
Yang Zhang <youngzh@umich.edu>
2020-05-07 11:25:12 -0700

    updated linearized model

--- scripts/simplified_linear_bicycle.py
+++ scripts/simplified_linear_bicycle.py
@@ -3,16 +3,18 @@ import math
 import numpy as np
 
 tandelta = math.tan(delta)
-angel = psi + math.atanh((lr*tandelta)/L)
-deno1 = tan(delta)^2 + 1
-deno2 = (lr^2*tandelta^2)/L^2 + 1
+angel = yaw + math.atanh((self.lr*tandelta)/self.L)
+deno1 = np.tan(delta)**2 + 1
+deno2 = (self.lr**2*tandelta**2)/self.L**2 + 1
+deno3 = self.L * np.sqrt(deno2)
 
-A = np.array([[ 0, 0, np.cos(angel), -v*np.sin(angel)],
-              [ 0, 0, np.sin(angel),  v*np.cos(angel)],
-              [ 0, 0, 0, 0],
-              [ 0, 0, tandelta/(L*(deno2)^(1/2)), 0]]
+A_temp = np.array([[ 0, 0, np.cos(angel), -v*np.sin(angel)],
+            [ 0, 0, np.sin(angel),  v*np.cos(angel)],
+            [ 0, 0, 0, 0],
+            [ 0, 0, tandelta/deno3, 0]]) * dt
+A_temp = A_temp + np.eye(4)
 
-B = np.array([[ 0, -(lr*v*np.sin(angel)*(deno1))/(L*(deno2))],
-              [ 0, (lr*v*np.cos(angel)*(deno1))/(L*(deno2))],
-              [ 1, 0],
-              [ 0, (v*(deno1))/(L*(deno2)^(1/2)) - (lr^2*v*tandelta^2*(deno1))/(L^3*(deno2)^(3/2))]]
+B_temp = np.array([[ 0, -(self.lr*v*np.sin(angel)*(deno1))/(self.L*(deno2))],
+            [ 0, (self.lr*v*np.cos(angel)*(deno1))/(self.L*(deno2))],
+            [ 1, 0],
+            [ 0, (v*(deno1))/deno3 - (self.lr**2*v*tandelta**2*(deno1))/(deno3**3)]]) * dt
